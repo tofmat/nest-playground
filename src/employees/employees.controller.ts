31c59d9fc1e567da79bdbd12b67469a575c4c92c
Tosin <tofmatt@gmail.com>
2024-09-21 22:25:32 +0100

    nest dope

--- src/employees/employees.controller.ts
+++ src/employees/employees.controller.ts
@@ -28,7 +28,10 @@ export class EmployeesController {
   @SkipThrottle({ default: false })
   @Get()
   findAll(@Ip() ip: string, @Query('role') role?: 'INTERN' | 'ADMIN') {
-    this.logger.log(`Request for all employees\t${ip}`);
+    this.logger.log(
+      `Request for all employees\t${ip}`,
+      EmployeesController.name,
+    );
     return this.employeesService.findAll(role);
   }
 
